ef80b934b53cf2ceb7d40a9ddc5945497d451ccf
Anni Mustonen <anni.mustonen@tuni.fi>
2025-02-18 13:57:08 +0200

    deleted moi.js

--- moi.js
+++
@@ -1,6 +0,0 @@
-const clientID = "0672070138953547";  // Vaihda omaan Client ID:hen
-const clientSecret = "nQFQxWn1u1ETwqgpnJJV91fnMwkKKaC0";  // 🔹 Vaihda omaan Client Secret:iin
-const encodedCredentials = btoa(`${clientID}:${clientSecret}`);
-
-console.log(encodedCredentials);
-// MDY3MjA3MDEzODk1MzU0NzpuUUZReFduMXUxRVR3cWdwbkpKVjkxZm5Nd2tLS2FDMA==
